f77a8b9058dd86a5b8a8379fd9c2c4d17e186a44
mehranfarhdi <mehran.farhadi@aut.ac.ir>
2023-03-05 16:42:23 +0330

    as

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,3 +1,7 @@
 python manage.py makemigrations
+python manage.py makemigrations tread
+python manage.py migrate tread
+python manage.py makemigrations transaction
+python manage.py migrate transaction
 python manage.py migrate
 python3 manage.py runserver
